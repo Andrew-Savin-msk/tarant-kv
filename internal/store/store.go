41cfc420c02ea6fb80df6df599c9030daf60c92c
Andrew-Savin-msk <4andreysavin@gmail.com>
2024-08-20 11:31:18 +0300

    Product is ready

--- internal/store/store.go
+++ internal/store/store.go
@@ -13,9 +13,7 @@ type ValueStore interface {
 	SetKeys(data map[string]interface{}) ([]Bind, error)
 	GetKeys(keys []string) (map[string]interface{}, []string, error)
 }
-
+ 
 type UserStore interface {
-	// SaveUser(login string, pHash []byte) error
 	FindUser(login string) (*models.User, error)
-	// SaveToken(login, token string) error
 }
